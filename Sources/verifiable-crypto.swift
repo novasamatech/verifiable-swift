7889b5b105efcab1d6c4bd2a344178ad620b81e6
Krzysztof Rodak <krodak.konta@gmail.com>
2024-06-11 12:49:23 +0200

    feat: update lib

--- Sources/verifiable-crypto.swift
+++ Sources/verifiable-crypto.swift
@@ -1,10 +1,29 @@
 import verifiable_crypto
 
 func internalDeriveMemberKey<GenericIntoRustString: IntoRustString>(_ entropy: GenericIntoRustString) -> RustString {
-    RustString(ptr: __swift_bridge__$derive_member_key({ let rustString = entropy.intoRustString(); rustString.isOwned = false; return rustString.ptr }()))
+    RustString(ptr: __swift_bridge__$derive_member_key(prepareRustString(entropy)))
 }
+
 func internalCreateProof<GenericIntoRustString: IntoRustString>(_ entropy: GenericIntoRustString, _ members: RustVec<GenericIntoRustString>, _ context: GenericIntoRustString, _ message: GenericIntoRustString) -> RustString {
-    RustString(ptr: __swift_bridge__$create_proof({ let rustString = entropy.intoRustString(); rustString.isOwned = false; return rustString.ptr }(), { let val = members; val.isOwned = false; return val.ptr }(), { let rustString = context.intoRustString(); rustString.isOwned = false; return rustString.ptr }(), { let rustString = message.intoRustString(); rustString.isOwned = false; return rustString.ptr }()))
+    RustString(ptr: __swift_bridge__$create_proof(
+        prepareRustString(entropy),
+        { let val = members; val.isOwned = false; return val.ptr }(),
+        prepareRustString(context),
+        prepareRustString(message)
+    ))
 }
 
+func internalSign<GenericIntoRustString: IntoRustString>(_ entropy: GenericIntoRustString, _ message: GenericIntoRustString) -> RustString {
+    RustString(
+        ptr: __swift_bridge__$sign(
+            prepareRustString(entropy),
+            prepareRustString(message)
+        )
+    )
+}
 
+private func prepareRustString<GenericIntoRustString: IntoRustString>(_ value: GenericIntoRustString) -> UnsafeMutablePointer<CChar> {
+    let rustString = value.intoRustString()
+    rustString.isOwned = false
+    return rustString.ptr.assumingMemoryBound(to: CChar.self)
+}
--- bindings/xcframework/verifiable_crypto.xcframework/ios-arm64/Headers/verifiable-crypto.h
+++ bindings/xcframework/verifiable_crypto.xcframework/ios-arm64/Headers/verifiable-crypto.h
@@ -2,5 +2,6 @@
 #include <stdint.h>
 void* __swift_bridge__$derive_member_key(void* entropy);
 void* __swift_bridge__$create_proof(void* entropy, void* members, void* context, void* message);
+void* __swift_bridge__$sign(void* entropy, void* message);
 
 
